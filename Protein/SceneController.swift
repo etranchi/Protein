902241a08fa838d0197bbe727a904a80ea039922
etranchi <etranchi@student.42.fr>
2019-01-06 18:02:48 +0100

    ptite modif

--- Protein/SceneController.swift
+++ Protein/SceneController.swift
@@ -22,6 +22,8 @@ class SceneController: UIViewController {
     var scnScene: SCNScene!
     var cameraNode: SCNNode!
     var isRotating = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
@@ -140,7 +142,7 @@ class SceneController: UIViewController {
     func setupView() {
         
         scnView = SCNView()
-        // scnView.showsStatistics = true
+        scnView.showsStatistics = true
         scnView.allowsCameraControl = true
         
         let gesture = UITapGestureRecognizer(target: self, action: #selector(tapGesture))
@@ -184,41 +186,30 @@ class SceneController: UIViewController {
     func setupScene() {
         scnScene = SCNScene()
         scnScene.fogColor = UIColor.clear
-        
         scnView.scene = scnScene
     }
     
     func setupCamera() {
-        // 1
         cameraNode = SCNNode()
-        // 2
         cameraNode.camera = SCNCamera()
-        // 3
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 150)
-        // 4
         cameraNode.camera?.zFar = 500
         scnScene.rootNode.addChildNode(cameraNode)
     }
     
     func setupShapes(_ sh : [Shape]) {
-        
         for d in sh {
             let sphere = SCNSphere(radius: 0.5)
             var color : UIColor
             switch d.type {
-            case "H" : color = UIColor(white: 1, alpha: 1)
+            case "H" : color = .white
             case "C" : color = .black
             case "N": color = .blue
             case "O" : color = .red
             case "F","Cl" : color = .green
-            case "Br" : color = UIColor(red: 153, green: 34, blue: 0, alpha: 1)
-            case "I" : color = UIColor(red: 102, green: 0, blue: 187, alpha: 1)
-            case "He","Ne","Ar","Xe","Kr" : color = UIColor(red: 0, green: 255, blue: 255, alpha: 1)
             case "P" : color = UIColor.orange
             case "S" : color = .yellow
-            case "B" : color = UIColor(red: 255, green: 170, blue: 119, alpha: 1)
             case "Li","Na","K","Rb","Cs","Fr" : color = .purple
-            case "Be","Mg","Ca","Sr","Ba","Ra" : color = UIColor(red: 153, green: 153, blue: 153, alpha: 1)
             case "Ti" : color = .gray
             default : color = .black
             }
@@ -229,10 +220,6 @@ class SceneController: UIViewController {
             colorShapes.append((d.id, color))
             scnScene.rootNode.addChildNode(node)
         }
-    }
-    
-    func posBetween(first: SCNVector3, second: SCNVector3) -> SCNVector3 {
-        return SCNVector3Make((first.x + second.x) / 2, (first.y + second.y) / 2, (first.z + second.z) / 2)
     }
     
     
@@ -250,34 +237,15 @@ class SceneController: UIViewController {
             let s2 = colorShapes.first(where: { (id,color) -> Bool in
                 return id == to!.id ? true : false
             })
-            let ndLine = CylinderNode(
-                parent: scnScene.rootNode, // ** needed
-                s1: from!,    // line (cylinder) starts here
-                s2: to!,    // line ends here
-                radius: 0.2,   // line thickness
-                radSegmentCount: 6,
-                s1Color : s1!.1,
-                s2Color : s2!.1
-            )  // any material
+            let cylinder = Cylinder(parent: scnScene.rootNode, s1: from!, s2: to!, radius: 0.2, radSegmentCount: 6, s1Color : s1!.1, s2Color : s2!.1)
             
-            scnScene.rootNode.addChildNode(ndLine)
+            scnScene.rootNode.addChildNode(cylinder)
         }
     }
 }
 
 
 extension SCNVector3 {
-    /**
-     * Calculates the cross product between two SCNVector3.
-     */
-    func cross(vector: SCNVector3) -> SCNVector3 {
-        return SCNVector3Make(y * vector.z - z * vector.y, z * vector.x - x * vector.z, x * vector.y - y * vector.x)
-    }
-    
-    func length() -> Float {
-        return sqrtf(x*x + y*y + z*z)
-    }
-    
     func distance(_ receiver:SCNVector3) -> Float{
         let xd = receiver.x - self.x
         let yd = receiver.y - self.y
@@ -285,38 +253,19 @@ extension SCNVector3 {
         let distance = Float(sqrt(xd * xd + yd * yd + zd * zd))
         return (distance)
     }
-    
-    /**
-     * Normalizes the vector described by the SCNVector3 to length 1.0 and returns
-     * the result as a new SCNVector3.
-     */
-    func normalized() -> SCNVector3 {
-        return self / length()
-    }
-    
-    static func + (left : SCNVector3, right : SCNVector3) -> SCNVector3 {
-        return SCNVector3Make(left.x + right.x, left.y + right.x, left.z + right.z)
-    }
-    static func - (left : SCNVector3, right : SCNVector3) -> SCNVector3 {
-        return SCNVector3Make(left.x - right.x, left.y - right.x, left.z - right.z)
-    }
-    
-    static func / (vector: SCNVector3, scalar: Float) -> SCNVector3 {
-        return SCNVector3Make(vector.x / scalar, vector.y / scalar, vector.z / scalar)
-    }
 }
 
 
-class CylinderNode: SCNNode
+class Cylinder: SCNNode
 {
-    init( parent: SCNNode,     // because this node has not yet been assigned to a parent.
-        s1: Shape,  // where line starts
-        s2: Shape,  // where line ends
-        radius: CGFloat,     // line thicknes
-        radSegmentCount: Int,
-        s1Color : UIColor,
-        s2Color : UIColor
-        )  // any material.
+    init( parent: SCNNode,
+          s1: Shape,
+          s2: Shape,
+          radius: CGFloat,
+          radSegmentCount: Int,
+          s1Color : UIColor,
+          s2Color : UIColor
+        )
     {
         super.init()
         let v1 = s1.pos
@@ -324,36 +273,29 @@ class CylinderNode: SCNNode
         let height = v1.distance(v2)
         
         position = v1
+        let dif = v1.z - v2.z
+        print(dif)
+        print(v1)
+        print(v2)
         
-    
+        let node = SCNNode()
+        node.position = v2
+        parent.addChildNode(node)
         
+        let ZAlign = SCNNode()
+        ZAlign.eulerAngles.x = .pi / 2
         
-        let ndV2 = SCNNode()
-        ndV2.position = v2
-        parent.addChildNode(ndV2)
-//        let ndV22 = SCNNode()
-//        ndV22.position = v1
-//        parent.addChildNode(ndV2)
+        let geo = SCNCylinder(radius: radius, height: CGFloat(height / 2))
+        geo.radialSegmentCount = radSegmentCount
+        geo.firstMaterial!.diffuse.contents = s1Color
+        let nodeCyl = SCNNode(geometry: geo)
+        nodeCyl.position.y = -height / 4
         
-        let ndZAlign = SCNNode()
-        ndZAlign.eulerAngles.x = .pi / 2
+        ZAlign.addChildNode(nodeCyl)
         
-        let cylgeo = SCNCylinder(radius: radius, height: CGFloat(height / 2))
-        cylgeo.firstMaterial!.diffuse.contents = s2Color
-        cylgeo.radialSegmentCount = radSegmentCount
+        addChildNode(ZAlign)
         
-        let ndCylinder1 = SCNNode(geometry: cylgeo )
-        ndCylinder1.position.y = -height / 4
-        ndZAlign.addChildNode(ndCylinder1)
-        
-        cylgeo.firstMaterial!.diffuse.contents = s1Color
-        /*let ndCylinder2 = SCNNode(geometry: cylgeo )
-        ndCylinder2.position.y = -(height * 3) / 4
-        ndZAlign.addChildNode(ndCylinder2)*/
-        
-        addChildNode(ndZAlign)
-        
-        constraints = [SCNLookAtConstraint(target: ndV2)]
+        constraints = [SCNLookAtConstraint(target: node)]
     }
     
     override init() {
